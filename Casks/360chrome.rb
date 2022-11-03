283e981e287de3cbd88a722bb9920d19bd23d90b
zhaopeng <imzhpe@qq.com>
2022-11-04 00:44:59 +0800

    Update 360chrome.rb

--- Casks/360chrome.rb
+++ Casks/360chrome.rb
@@ -1,9 +1,10 @@
 cask "360chrome" do
-  sha256 "70e32156e8bbf9cba58af09decf3a533d9b7639eec2208af2b99e8296c623b0b"
   version "12.2.1662.0"
+  sha256 :no_check # required as upstream package is updated in-place
+
   url "https://zhaopengme-generic.pkg.coding.net/macapps/public/360Chrome.dmg"
   name "360chrome"
   homepage "https://browser.360.cn/ee/mac/index.html"
 
-  app "360极速浏览器.app"
+  app "360chrome.app"
 end
