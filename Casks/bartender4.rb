63c26d7e291180dffbb9aa7ce755e2704a98550c
zhaopeng <imzhpe@qq.com>
2022-11-04 00:52:48 +0800

    Create bartender4.rb

---
+++ Casks/bartender4.rb
@@ -0,0 +1,9 @@
+cask "bartender4" do
+  version "0.0.1"
+  sha256 :no_check
+
+  url "https://zhaopengme-generic.pkg.coding.net/macapps/public/Bartender4.dmg"
+  name "bartender4"
+
+  app "bartender4.app"
+end
