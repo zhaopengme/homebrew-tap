dda81eca35458e9599bf945a9d0f3cd30eda301f
zhaopeng <imzhpe@qq.com>
2022-11-05 02:56:10 +0800

    Update sshpass.rb

--- sshpass.rb
+++ sshpass.rb
@@ -2,11 +2,11 @@ class Sshpass < Formula
   desc "sshpass with TOTP support"
   homepage "https://github.com/dora38/sshpass"
   version "1.09"
+  sha256 :no_check # required as upstream package is updated in-place
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/zhaopengme/homebrew-tap/releases/download/0.0.1/sshpass"
-      sha256 :no_check
 
       def install
         bin.install "sshpass"
